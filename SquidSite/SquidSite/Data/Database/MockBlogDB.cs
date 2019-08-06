f97f641d6a28489e4083743b70cc3f0d370fc802
Thumpax <c.t.raines.iii@gmail.com>
2019-08-05 19:49:29 -0600

    Blog Interface and MockDB

---
+++ SquidSite/SquidSite/Data/Database/MockBlogDB.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SquidSite.Database.Interfaces;
+using SquidSite.Models;
+
+namespace SquidSite.Data.Database
+{
+    public class MockBlogDB : IBlogDAL
+    {
+        public static List<BlogPost> mockDB = new List<BlogPost>()
+        {
+            new BlogPost() { title="Blog1", content="TestBlog1", posterUserName="Test", datePosted=DateTime.Now },
+            new BlogPost() { title="Blog2", content="TestBlog2", posterUserName="Test", datePosted=DateTime.Now },
+            new BlogPost() { title="Blog3", content="TestBlog3", posterUserName="Test", datePosted=DateTime.Now },
+        };
+
+        public bool AddBlog(BlogPost blog)
+        {
+            mockDB.Add(blog);
+            return true;
+        }
+
+        public bool DeleteBlog(int index)
+        {
+            mockDB.RemoveAt(index);
+            return true;
+        }
+
+        public bool DeleteBlog(BlogPost blog)
+        {
+            mockDB.Remove(blog);
+            return true;
+        }
+
+        public bool EditBlog(int index, BlogPost blog)
+        {
+            mockDB[index] = blog;
+            return true;
+        }
+
+        public IEnumerable<BlogPost> Filter(BlogPost.eBlogTag tag)
+        {
+            return mockDB.Where(b => b.tag.HasFlag(tag));
+        }
+
+        public IEnumerable<BlogPost> GetAll()
+        {
+            return mockDB;
+        }
+
+        public BlogPost GetBlog(int index)
+        {
+            return mockDB[index];
+        }
+
+        public int GetIndex(BlogPost blog)
+        {
+            for(int i = 0; i < mockDB.Count; i++)
+            {
+                if(mockDB[i] == blog)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public IEnumerable<BlogPost> GetPinned()
+        {
+            return mockDB.Where(b => b.tag.HasFlag(BlogPost.eBlogTag.PINNED));
+        }
+
+        public IEnumerable<BlogPost> Search(string Title)
+        {
+            return mockDB.Where(b => b.title.Contains(Title));
+        }
+    }
+}
---
+++ SquidSite/SquidSite/Data/Interfaces/IBlogDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SquidSite.Models;
+
+namespace SquidSite.Database.Interfaces
+{
+
+
+    public interface IBlogDAL
+    {
+        IEnumerable<BlogPost> GetAll();
+        IEnumerable<BlogPost> GetPinned();
+        IEnumerable<BlogPost> Search(string Title);
+        IEnumerable<BlogPost> Filter(BlogPost.eBlogTag tag);
+
+        bool AddBlog(BlogPost blog);
+        bool DeleteBlog(int index);
+        bool DeleteBlog(BlogPost blog);
+        bool EditBlog(int index, BlogPost blog);
+        BlogPost GetBlog(int index);
+        int GetIndex(BlogPost blog);
+    }
+}
