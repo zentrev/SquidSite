805f17dd2f3e2b7424fd5014835dbd583eded2a9
Spencer <southam.spencer.d@gmail.com>
2019-08-12 19:28:29 -0600

    implemented 2 tables

--- SquidSite/Tables/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].Comment
-(
-	[CommentId] INT NOT NULL PRIMARY KEY, 
-    [WriterId] INT NOT NULL, 
-    [BlogId] INT NOT NULL, 
-    [DateCreated] DATETIME NOT NULL, 
-    [DateEdited] DATETIME NOT NULL, 
-    [Content] NCHAR(1000) NOT NULL
-)
