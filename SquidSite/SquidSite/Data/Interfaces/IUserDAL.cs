1fc840e498383fb46c274e98328320063d253609
Thumpax <c.t.raines.iii@gmail.com>
2019-08-14 19:14:24 -0600

    Dal's should be done

---
+++ SquidSite/SquidSite/Data/Interfaces/IUserDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SquidSite.Models;
+
+namespace SquidSite.Data.Interfaces
+{
+    interface IUserDAL
+    {
+        IEnumerable<User> GetAll();
+        IEnumerable<User> Search(string name);
+        IEnumerable<User> Search(int ID);
+        IEnumerable<User> Filter(User.eUserType userType);
+
+        bool ValidateUser(string userName, string password);
+        bool AddUser(User user);
+        bool DeleteUser(int Key);
+        bool DeleteUser(User blog);
+        bool EditUSer(int key, User user);
+        BlogPost GetUser(int key);
+        int GetKey(User blog);
+    }
+}
