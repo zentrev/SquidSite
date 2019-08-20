31ea920ae245e7c8d537e96ba4b8cd27a2d756be
Thumpax <c.t.raines.iii@gmail.com>
2019-08-20 08:07:32 -0600

    BCrypt.cs

---
+++ SquidSite/SquidSite/DataEncryption/BCrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace SquidSite.DataEncryption
+{
+    public static class _BCrypt
+    {
+        private const int WF = 18;
+
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(WF);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, correctHash);
+        }
+    }
+}
