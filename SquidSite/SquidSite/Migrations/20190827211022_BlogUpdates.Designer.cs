7714b21ac3fbb2534eb1513b7837e340453cdd24
Rowan <eremy@student.neumont.edu>
2019-08-27 15:13:11 -0600

    added new migration

--- SquidSite/SquidSite/Migrations/20190827211022_BlogUpdates.Designer.cs
+++ SquidSite/SquidSite/Migrations/20190827211022_BlogUpdates.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SquidSite.Data.Database;
 
 namespace SquidSite.Migrations
 {
     [DbContext(typeof(SquidSiteDbContext))]
-    partial class SquidSiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190827211022_BlogUpdates")]
+    partial class BlogUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
