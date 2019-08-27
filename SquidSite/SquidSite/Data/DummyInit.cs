577d919bc3862f32c39fa85f4c6bdf74b4b918ef
Thumpax <c.t.raines.iii@gmail.com>
2019-08-27 17:20:38 -0600

    DummyInit for Products

--- SquidSite/SquidSite/Data/DummyInit.cs
+++ SquidSite/SquidSite/Data/DummyInit.cs
@@ -13,7 +13,10 @@ namespace SquidSite.Data
         {
             if (!context.Products.Any())
             {
-                context.ProductImage.Add(new ProductImages() { Product = new Product() { Title = "TestGame", Cost = 10.11f, Description = "Testing Game", DownloadLink = "download.exe", DemoLink = "demo here", DemoDownloadLink="ima a demo download" }, ProductImage = new ProdutImage() { ImageURL = "NULL.png"} });
+                context.ProductImage.Add(new ProductImages() { Product = new Product() { Title = "TestGame", Cost = 10.11f, Description = "Testing Game", DownloadLink = "download.exe", DemoLink = "demo here", DemoDownloadLink="ima a demo download", ProductType = Product.eProductType.GAME }, ProductImage = new ProdutImage() { ImageURL = "NULL.png"} });
+                context.ProductImage.Add(new ProductImages() { Product = new Product() { Title = "TestGame2", Cost = 10.91f, Description = "Testing Game2", DownloadLink = "download.exe", DemoLink = "demo here", ProductType = Product.eProductType.GAME }, ProductImage = new ProdutImage() { ImageURL = "NULL.png"} });
+                context.ProductImage.Add(new ProductImages() { Product = new Product() { Title = "TestGame3", Cost = 14.11f, Description = "Testing Game3", DownloadLink = "download.exe", ProductType = Product.eProductType.GAME }, ProductImage = new ProdutImage() { ImageURL = "NULL.png"} });
+                context.ProductImage.Add(new ProductImages() { Product = new Product() { Title = "Cool Socks", Cost = 36.99f, Description = "some really cool knited socks", MerchTags=Product.eMerchTags.SOCKS, ProductType = Product.eProductType.MERCH }, ProductImage = new ProdutImage() { ImageURL = "NULL.png"} });
             }
         }
     }
